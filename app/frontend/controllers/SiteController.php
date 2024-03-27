80d84b83093dada183025eb23bc5504f870b4196
sergalas <hudos@rambler.ru>
2024-03-27 12:48:11 +0300

    add all

---
+++ app/frontend/controllers/SiteController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\Modules\Image\Services\ImageService;
+use frontend\models\FileModel;
+use frontend\models\ResendVerificationEmailForm;
+use frontend\models\VerifyEmailForm;
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+use yii\web\UploadedFile;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionError(){
+        return $this->render('error');
+    }
+
+
+}
